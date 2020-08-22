1e09768652eded7a095d2903d91409b793a663e9
Gunjan P Deotale <gdeotale@gmail.com>
2020-08-22 22:50:39 +0530

    Assignment 4

--- Assignment4/Datahtml/js/upload_4.js
+++ Assignment4/Datahtml/js/upload_4.js
@@ -1,21 +1,14 @@
 function uploadAndClassifyImage(){
-	var fileInput = document.getElementById('FaceMergeFileUpload').files;
+	var fileInput = document.getElementById('faceFileUpload').files;
 	if(!fileInput.length){
-		return alert('please choose o upload first image');
-	}
-	var fileInput1 = document.getElementById('FaceMergeFileUpload1').files;
-	if(!fileInput1.length){
-		return alert('please choose to upload second image');
+		return alert('please choose file to upload first');
 	}
 	
 	var file = fileInput[0];
 	var filename = file.name;
-	var file1 = fileInput1[0];
-	var filename1 = file1.name;
 
 	var formData = new FormData();
 	formData.append(filename, file);
-	formData.append(filename1, file1);
 
 	console.log(filename);
 
@@ -23,7 +16,7 @@ function uploadAndClassifyImage(){
 		async: true,
 		crossDomain: true,
 		method: 'POST',
-		url: 'https://uygiqqxfqc.execute-api.ap-south-1.amazonaws.com/dev/align',
+		url: 'https://9nnncm80a9.execute-api.ap-south-1.amazonaws.com/dev/classify',
 		data: formData,
 		processData: false,
 		contentType: false,
@@ -31,10 +24,9 @@ function uploadAndClassifyImage(){
 	})	
 	.done(function(response){
 		console.log(response);
-		var image = document.getElementById('output6');
-		image.src = "https://session3-face-alignment-face-swap.s3.amazonaws.com/face-swap.png?t=" + new Date().getTime();
+		document.getElementById('result5').textContent = response;
 	})
-	.fail(function(){alert("There was an error while sending prediction request to align model");});
+	.fail(function(){alert("There was an error while sending prediction request to Mobilenet model");});
 	};
 	
-    $('#btnFaceMergeUpload').click(uploadAndClassifyImage);
+    $('#btnFaceUpload').click(uploadAndClassifyImage);
